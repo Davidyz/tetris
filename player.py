e8325b2a9e47c521fefc48048df88e79e87d70ba
ZheYU <zcabzyu@ucl.ac.uk>
2020-11-19 00:39:05 +0000

    New coefficients.

--- player.py
+++ player.py
@@ -52,7 +52,7 @@ class Candidate():
         self.weight = 100 # lower is better
 
     def cal_weight(self):
-        coefficients = [1000,                    1,                      10,                          15,                            5,                          -10000]
+        coefficients = [1000,                    1,                      11,                          15,                            10,                          -10000]
         parameters = [self.get_holes() / 230 , self.get_range() / 23, self.get_var_height() / 144, self.get_mean_height() / 24, self.get_bottom_holes() / 10, self.score / 16]
         self.weight = sum(coefficients[i] * parameters[i] for i in range(len(coefficients)))
         return self.weight
